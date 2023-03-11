5116c6ca55273fbb1bf8af1884d12d8616b84cec
Stefan Vitoria <steph.74lw@gmail.com>
2023-03-11 15:20:26 -0100

    done project basics configurations and structure setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
+import styles from "./styleConstants";
 
-const App = ()=> {
-  return (<div>
-    <h1>App</h1>
-  </div>)
-}
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <header className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>NavBar</div>
+      </header>
+
+      <section className={`bg-primary ${styles.flexStart}`}>
+        <section className={`${styles.boxWidth}`}>Hero</section>
+      </section>
+      <section className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <section className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+        </section>
+      </section>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+};
 
 export default App;

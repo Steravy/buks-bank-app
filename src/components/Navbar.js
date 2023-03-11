f56b3ba45c6835432fe0377438acb70c868564c7
Stefan Vitoria <steph.74lw@gmail.com>
2023-03-11 18:02:58 -0100

    done mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { navLinks } from "../constants/staticTexts";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-  
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hooBank" className="w-[124px] h-[32px]" />
@@ -13,14 +13,39 @@ const Navbar = () => {
         {navLinks.map((navLink, index) => (
           <li
             key={navLink.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length-1 ? 'mr-0' : 'mr-10'}`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
           >
             <a href={`#${navLink.id}`}>{navLink.title}</a>
           </li>
         ))}
       </ul>
-      <div className="sm:hidden flex flex-1 justify-end items-center" >
-        <img src={toggle ? close : menu } alt="menu" className="w-[28px] h-[28px] object-contain" />
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prevValue) => !prevValue)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((navLink, index) => (
+              <li
+                key={navLink.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                }`}
+              >
+                <a href={`#${navLink.id}`}>{navLink.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );

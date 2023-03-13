7c2dea6c96e90aa9d9771be01ddea31b3504bf7a
Stefan Vitoria <steph.74lw@gmail.com>
2023-03-12 23:33:10 -0100

    start working on business component

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles, { layout } from "../styleConstants";
+import Button from "./Button";
 
 const Business = () => {
   return (
-    <div>Business</div>
-  )
-}
+    <section id="features" className={`${layout.section}`}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the Business, <br className="sm:block hidden" /> we'll handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by buy
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+      </div>
+      <Button styles="mt-10" />
+    </section>
+  );
+};
 
-export default Business
+export default Business;

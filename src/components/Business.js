50d37207bfd9e4769b692a721f78bc6b45b34972
Stefan Vitoria <steph.74lw@gmail.com>
2023-03-30 21:43:09 -0100

    start working on Features component

--- src/components/Business.js
+++ src/components/Business.js
@@ -4,7 +4,7 @@ import Button from "./Button";
 import { features } from "../constants/staticTexts";
 
 
-const FeatureCard = () => {
+const FeatureCard = ({ icon, title, content, index}) => {
   return (
     <div>
 

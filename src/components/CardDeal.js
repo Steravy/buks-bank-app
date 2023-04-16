afd4a60f90e21ec51a40cc8f020705dba6e01af8
Stefan Vitoria <steph.74lw@gmail.com>
2023-04-16 12:01:50 -0100

    start working on CardDeal

--- src/components/CardDeal.js
+++ src/components/CardDeal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const CardDeal = () => {
   return (
@@ -6,4 +6,4 @@ const CardDeal = () => {
   )
 }
 
-export default CardDeal
+export default CardDeal;

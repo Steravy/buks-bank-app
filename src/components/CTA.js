6e69e33e2690c117b1d6cd8bfa852f89fa08ef14
Stefan Vitoria <steph.74lw@gmail.com>
2023-04-16 16:26:10 -0100

    finishing working on CTA section

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styleConstants';
+import Button from './Button';
+
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <secction className={`${styles.padding} ${styles.flexCenter} ${styles.marginY} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`} >
+      <div className={`flex-1 flex flex-col`} >
+        <h2 className={`${styles.heading2}`} >
+          Let`s try our service now!
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`} >
+          Cupidatat velit voluptate irure sit nisi et pariatur voluptate minim ipsum aliquip.
+          Mollit aute irure elit eu laboris.
+        </p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`} >
+        <Button styles={styles} />
+      </div>
+    </secction>
   )
 }
 
